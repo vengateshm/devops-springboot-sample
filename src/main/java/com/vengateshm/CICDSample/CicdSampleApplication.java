bf7031a94cffad47caaf6a1db7d748dae3754ced
vengateshm <40466166+vengateshm@users.noreply.github.com>
2022-08-06 21:44:10 +0530

    initial commit

---
+++ src/main/java/com/vengateshm/CICDSample/CicdSampleApplication.java
@@ -0,0 +1,13 @@
+package com.vengateshm.CICDSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSampleApplication.class, args);
+	}
+
+}

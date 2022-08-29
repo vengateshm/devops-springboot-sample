c29057b5d5b860aaa6018b0928ca7b4c3feec8f4
vengateshm <40466166+vengateshm@users.noreply.github.com>
2022-08-29 22:36:19 +0530

    get endpoint added

--- src/main/java/com/vengateshm/CICDSample/CicdSampleApplication.java
+++ src/main/java/com/vengateshm/CICDSample/CicdSampleApplication.java
@@ -2,12 +2,21 @@ package com.vengateshm.CICDSample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CicdSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CicdSampleApplication.class, args);
-	}
+    @GetMapping
+    public String getMessage() {
+        return "Application deployed successfully!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("From main function!");
+        SpringApplication.run(CicdSampleApplication.class, args);
+    }
 
 }
